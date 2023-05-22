2758d215138cd557ad9569778f7d764af2acd6b4
system.conf <73934647+system-conf@users.noreply.github.com>
2023-05-22 11:40:49 +0300

    php deneme

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basit Toplama İşlemi</title>
+</head>
+<body>
+    <h1>Basit Toplama İşlemi</h1>
+
+    <?php
+    // İki sayıyı topla
+    $sayi1 = 5;
+    $sayi2 = 3;
+    $toplam = $sayi1 + $sayi2;
+
+    // Sonucu ekrana yazdır
+    echo "<p>$sayi1 + $sayi2 = $toplam</p>";
+    ?>
+</body>
+</html>
